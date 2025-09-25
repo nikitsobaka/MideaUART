120572940a5a5dfced7d531b910151105e5ff45b
Nikita.Krasyuk <Nikita.Krasyuk@jetbrains.com>
2025-09-25 11:47:11 +0200

    rer

--- include/Appliance/AirConditioner/AirConditioner.h
+++ include/Appliance/AirConditioner/AirConditioner.h
@@ -9,7 +9,7 @@ namespace dudanov {
 namespace midea {
 namespace ac {
 
-// Air conditioner control command test2 2234222
+// Air conditioner control command test2 2234222 33
 struct Control {
   Optional<float> targetTemp{};
   Optional<Mode> mode{};
